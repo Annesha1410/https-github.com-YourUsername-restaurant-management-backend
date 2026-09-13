b49c0a377732722f672d8a77f784d53c86017fb1
Annesha1410 <annesha8211@gmail.com>
2026-09-13 20:50:44 +0530

    Fix CORS for Vercel

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ connectDB();
 app.use(
     cors({
         origin: ["http://localhost:5173",
+            "https://swad-house.vercel.app",
          "https://restaurant-management-frontend-m99ubur6s-abcd-bf56.vercel.app"]
     })
 );
